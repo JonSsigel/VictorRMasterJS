80e09309d0b21c8f3efb51fa0c3179c06dc71e98
jsgutierrez <jsgutierrez@mticonsultoria.com>
2026-02-02 08:47:31 -0600

    Doom y eventos.

--- javaScriptProfundidad/DOM/js/02-bom.js
+++ javaScriptProfundidad/DOM/js/02-bom.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // BOM para las opciones de la ventana del navegador.
 
 console.log(window.innerWidth);
@@ -8,14 +8,15 @@ console.log(window.innerHeight);
 console.log(screen.width);
 console.log(screen.height);
 
-// 
+//
 
-console.log(window.location);// locación del archivo en la computadora, eso no está chido. que al parecer es la url más que donde esta el archivo.
+console.log(window.location); // locación del archivo en la computadora, eso no está chido. que al parecer es la url más que donde esta el archivo.
 // y hasta solo cambiando desde consola.
-function redirecion(url){// Esto se invoca desde consola y te manda a donde quieras, jo jo, no no no me gusta esto.
-	window.location.href = url;
+function redirecion(url) {
+  // Esto se invoca desde consola y te manda a donde quieras, jo jo, no no no me gusta esto.
+  window.location.href = url;
 }
 
 function abrirVentana(url) {
-	window.open(url,"","whidth=400,height=300"); // Este abre una nueva pestaña en el navegador. y puede tener propiedades, con esas propiedades abre otra ventana como popup
+  window.open(url, "", "whidth=400,height=300"); // Este abre una nueva pestaña en el navegador. y puede tener propiedades, con esas propiedades abre otra ventana como popup
 }
--- javaScriptProfundidad/DOM/js/03-eventos.js
+++ javaScriptProfundidad/DOM/js/03-eventos.js
@@ -1,74 +1,74 @@
-'use strict'
+"use strict";
 
 //funciones que se ejecutan cuando el usuario hace algo.
 
 // Eventos del ratón.
 // Cuando damos click, pasar por el elemento y al salir del elemento
-window.addEventListener('load', () => {
-	var boton = document.querySelector('#boton');
+window.addEventListener("load", () => {
+  var boton = document.querySelector("#boton");
 
-	function cambiarColor() {
-		console.log('Click color')
-		var bg = boton.style.background;
-		if (bg == 'green') {
-			boton.style.background = 'red';
-		} else {
-			boton.style.background = 'green';
-		}
-		boton.style.padding = '10px';
-		boton.style.border = '1px solid #ccc';
+  function cambiarColor() {
+    console.log("Click color");
+    var bg = boton.style.background;
+    if (bg == "green") {
+      boton.style.background = "red";
+    } else {
+      boton.style.background = "green";
+    }
+    boton.style.padding = "10px";
+    boton.style.border = "1px solid #ccc";
+  }
 
-	}
+  // Se agregará un escuchador de eventos
+  //
+  // Evento click
 
-	// Se agregará un escuchador de eventos
-	//
-	// Evento click
+  boton.addEventListener("click", function () {
+    cambiarColor();
+    console.log(this);
+    this.style.border = "10px solid black";
+  });
 
-	boton.addEventListener('click', function () {
-		cambiarColor();
-		console.log(this);
-		this.style.border = '10px solid black';
-	});
+  // mouse over
 
-	// mouse over
+  boton.addEventListener("mouseover", () => {
+    boton.style.background = "yellow";
+  });
 
-	boton.addEventListener('mouseover', () => {
-		boton.style.background = 'yellow';
-	});
+  // mouse out
+  boton.addEventListener("mouseout", () => {
+    boton.style.background = "grey";
+  });
 
-	// mouse out
-	boton.addEventListener('mouseout', () => {
-		boton.style.background = 'grey';
-	});
+  // Evento focus
+  var input = document.querySelector("#campo_nombre");
+  input.addEventListener("focus", () => {
+    console.log("[focus]Estas dentro del input");
+  });
 
-	// Evento focus
-	var input = document.querySelector("#campo_nombre");
-	input.addEventListener('focus', () => {
-		console.log('[focus]Estas dentro del input');
-	});
+  // Evento blur
+  input.addEventListener("blur", () => {
+    console.log("[blur]Estas fuera del input");
+  });
 
-	// Evento blur
-	input.addEventListener('blur', () => {
-		console.log('[blur]Estas fuera del input');
-	});
+  /*// Evento keydown al pulsar una tecla*/
+  input.addEventListener("keydown", (event) => {
+    console.log(
+      "[keydown]pulsando una tecla",
+      String.fromCharCode(event.keyCode),
+    );
+  });
 
-	/*// Evento keydown al pulsar una tecla*/
-	input.addEventListener('keydown', (event) => {
-		console.log('[keydown]pulsando una tecla', String.fromCharCode(event.keyCode));
-	});
+  /*// Evento keypress*/
+  input.addEventListener("keypress", (event) => {
+    console.log(
+      "[keypress]Tecla presionada",
+      String.fromCharCode(event.keyCode),
+    );
+  });
 
-
-	/*// Evento keypress*/
-	input.addEventListener('keypress', (event) => {
-		console.log('[keypress]Tecla presionada', String.fromCharCode(event.keyCode));
-	});
-
-
-	/*// Evento keyup*/
-	input.addEventListener('keyup', () => {
-		console.log('[keyup]Tecla soltada');
-	});
-
-
+  /*// Evento keyup*/
+  input.addEventListener("keyup", () => {
+    console.log("[keyup]Tecla soltada");
+  });
 });
-
